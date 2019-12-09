6ed342da86ad5b08f702e8970c6a549fc9a8988b
iliassalmi <56584061+iliassalmi@users.noreply.github.com>
2019-12-09 09:20:24 +0100

    1

--- form.php
+++ form.php
@@ -20,6 +20,10 @@ $pays = $_GET['Pays'];
 $email = $_GET['email'];
 
 
+
+
+
+
 	$req = $bdd->prepare('INSERT INTO inscription_eleve (Nom, Prenom, Age , Pays, Mail) VALUES
 (:nom, :prenom, :Age, :pays, :mail)');
 	$a = $req->execute(array(
@@ -36,7 +40,7 @@ $email = $_GET['email'];
      $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
      $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
      $mail->Username   = 'iliasverif@gmail.com';                     // SMTP username
-     $mail->Password   = 'Tamere95';                               // SMTP password
+     $mail->Password   = 'mdp de ton adress';                               // SMTP password
      $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
      $mail->Port       = 587;                                    // TCP port to connect to
 
